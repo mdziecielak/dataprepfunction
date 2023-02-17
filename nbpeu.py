b9c6883ae85e59b4eefc9affb4bdfdea1c691223
mdziecielak <maciej.dziecielak@gmail.com>
2023-02-17 17:17:50 +0100

    Test

--- nbpeu.py
+++ nbpeu.py
@@ -3,4 +3,7 @@ import json
 api_url = "https://api.nbp.pl/api/exchangerates/rates/a/eur"
 response = requests.get(api_url)
 response.json()
-print(response.json())
+response.status_code
+data = json.loads(get_data())
+
+#print(response.json())
